d2727a3fe9e6e503c3c1097b303cb0fb910303d0
AayushiJ25 <aj25300jaiswal@gmail.com>
2024-06-16 20:21:30 +0530

    Adding files

---
+++ app.js
@@ -0,0 +1,7 @@
+let inp = document.querySelector("#text");
+let para = document.querySelector("p");
+
+inp.addEventListener("input", function() {
+  console.log(inp.value);
+  para.innerText = inp.value;
+});
